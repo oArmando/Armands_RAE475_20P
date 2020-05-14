544076c9c0d87608259b7d3879dc92f3943ea0ee
oArmando <armands.ostrovskis7@gmail.com>
2020-05-14 21:44:19 +0300

    izlabots

--- apachesetup2.sh
+++ apachesetup2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo nano /etc/apache2/sites-available/sampledomain.com.cona2ensite sampledomain.com.conf
+sudo nano /etc/apache2/sites-available/sampledomain.com.conf
+a2ensite sampledomain.com.conf
 sudo a2dissite 000-default.conf
 sudo systemctl restart apache2
 sudo apache2ctl configtest
