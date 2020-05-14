75581da1b0b7d2bb1a20bfe7d5c4036b633e720e
oArmando <armands.ostrovskis7@gmail.com>
2020-05-14 16:47:54 +0300

    pirmreizējais setups lai uzlitk programmu

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#this script is meant for installing neceesary programms on fresh machine
+sudo apt-get update
+
+sudo apt-get install python3 #install python3
+sudo apt-get install telnet #install telnet linux client
+sudo apt-get install apache2 #install apache2
+sudo apt-get install curl #install curl
+sudo apt-get install vsftpd  #install very secure ftp daemo
+echo "Installation Succesful"
+
+
