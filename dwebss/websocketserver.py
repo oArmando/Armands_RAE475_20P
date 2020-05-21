5f3ff4be5904e6ad93bef27450a5bd1d74ec4fbf
oArmando <armands.ostrovskis7@gmail.com>
2020-05-21 20:24:35 +0300

    dokerizeti websocket serveris un clients, divi compose faili, kur viens palaiž tikai serveri, bet otrs palaiz abus jaizmanto docker-compose 1.24

---
+++ dwebss/websocketserver.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 13:52:27 2020
+
+@author: armands
+"""
+import socket
+import time
+time.sleep(5)
+HEADERSIZE = 10 #set headersize
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#set TCP socket parameters
+host = socket.gethostbyname(socket.gethostname())
+port = 9095
+s.bind((host,port))#set host and port
+s.listen(5)#set to listenint
+(host):print("[ Server Started IP: "+host+" ]")
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection form {address} has been established!")#show in server.py connection
+    
+    msg = "Welcome to the server!" #send welcome message
+    msg= f'{len(msg):<{HEADERSIZE}}' +msg
+    
+    clientsocket.send(bytes(msg,"utf-8"))
+    
+    while True:#send time module
+        time.sleep(3)
+        msg = f"The time is {time.time()}"
+        msg= f'{len(msg):<{HEADERSIZE}}' +msg
+        clientsocket.send(bytes(msg,"utf-8"))
+    
